3b72a6629c05ce15339bddffac35cc0d1a7da692
Ritesh Raj <riteshraj26oct@gmail.com>
2020-08-07 18:18:25 +0000

    Update specs/task23.spec.js

--- specs/task23.spec.js
+++ specs/task23.spec.js
@@ -5,15 +5,42 @@ const make = require("../task21");
 const path = require("path");
 const syncCheck = require("./syncCheck.spec");
 
-describe("task 23 test running", () => {
-  it("Should delete a folder at given path with given name #end_test", done => {
+describe("FS Module testing #start_test", () => {
+  it("Should delete a folder at given path with given name", done => {
     let pathToDir = "./files/nayaFolder";
-    expect(1).to.equal(1);
-    done();
+    make(pathToDir).then(result => {
+      fstasks(pathToDir).then(userResult => {
+        expect(userResult).to.equal("File exists : Directory removed");
+        done();
+      });
+    });
   });
-  it("should not contain synchronous function",done => {
+  it("Should create and delete a folder at given path with given name", done => {
+    let pathToDir = "./files/nayaFolder";
+    fstasks(pathToDir).then(userResult => {
+      expect(userResult).to.equal("New directory created : Directory removed");
+      done();
+    });
+  });
+  it("Should delete a folder at given path with given name", done => {
+    let pathToDir = "./files/newFolder1";
+    make(pathToDir).then(result => {
+      fstasks(pathToDir).then(userResult => {
+        expect(userResult).to.equal("File exists : Directory removed");
+        done();
+      });
+    });
+  });
+  it("should not contain synchronous function", done => {
     syncCheck("./task23.js").then(res => {
       expect(res).to.equal(true);
+      done();
+    });
+  });
+  it("Should create and delete a folder at given path with given name #end_test", done => {
+    let pathToDir = "./files/newFolder6";
+    fstasks(pathToDir).then(userResult => {
+      expect(userResult).to.equal("New directory created : Directory removed");
       done();
     });
   });
