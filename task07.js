874e64679c639ed8591ca79f7cf00d12c6b849d7
madanadi0305 <madanadi0305@gmail.com>
2020-11-05 06:15:15 +0000

    Update Wiki/17-task07.md, task07.js files

---
+++ task07.js
@@ -0,0 +1,33 @@
+var fs=require('fs');
+function fetchFilesAndDirectories(dirPath){
+var countDir=0;
+var countFile=0; 
+return new Promise((res,rej)=>{
+fs.readdir(dirPath,(err,files)=>{
+if(err){
+    rej("Error occured while reading directory");
+}
+else{
+    var fileObj={};
+for(var i=0;i<files.length;i++){
+var fileElem=files[i];
+if(fs.stats.isFile(fileElem)===true){
+    countFile=countFile+1;
+}
+else{
+    countDir=countDir+1;
+}
+}
+   
+fileObj.countDir=countDir;
+fileObj.countFile=countFile;
+fileObj.files=files;
+    res(fileObj);
+}
+
+});
+
+});
+
+
+}
