2be443c21d823558180114ca6ee93af4bc86ca10
madanadi0305 <madanadi0305@gmail.com>
2020-11-05 06:46:22 +0000

    Update task07.js

--- task07.js
+++ task07.js
@@ -11,14 +11,22 @@ else{
     var fileObj={};
 for(var i=0;i<files.length;i++){
 var fileElem=files[i];
-var fileNameArray=fileElem.split('');
-for(var i=0;i<fileNameArray.length;i++){
-    if(String(fileNameArray[i])==='.'){
+//var fileNameArray=fileElem.split('');
+fs.stat(fileElem,(err,stats)=>{
+if(err){
+    throw err;
+}
+else{
+    if(stats.isFile(fileElem)){
         countFile=countFile+1;
     }
-    
+    else{
+        countDir=countDir+1;
+    }
 }
-countDir=countDir+1;
+
+});
+//countDir=countDir+1;
 }
    
 fileObj.countDir=countDir;
