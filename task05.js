f32cca179536392844563cd7dc7894fb466bf018
madanadi0305 <madanadi0305@gmail.com>
2020-11-05 05:03:40 +0000

    Update task05.js

--- task05.js
+++ task05.js
@@ -3,7 +3,7 @@ var path=require('path');
 function fetchFiles(dirPath){
 var finalDirectoryPath=path.dirname(dirPath);
 return new Promise((res,rej)=>{
-fs.readdir(path,(error,files)=>{
+fs.readdir(finalDirectoryPath,(error,files)=>{
 if(error){
     rej("Error occured while reading directory");
 }
