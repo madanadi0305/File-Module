ba80ec237bc53570a81117afa086e53569215add
madanadi0305 <madanadi0305@gmail.com>
2020-11-06 17:07:22 +0000

    Update Wiki/20-fs.readFIle.md, Wiki/21-task10.md, task10.js files

---
+++ task10.js
@@ -0,0 +1,22 @@
+var fs=require('fs');
+function fetchContent(pathToFile){
+return new Promise((res,rej)=>{
+fs.readfile(pathToFile,'utf-8',(err,data)=>{
+if(err){
+rej("Error reading file");
+
+}
+else{
+res(data.toString());
+
+}
+
+});
+
+
+});
+
+
+}
+
+module.exports=fetchContent;
