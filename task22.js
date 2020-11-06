33730e21042a7fb771cb6da2a810ed2d0c4fd57a
madanadi0305 <madanadi0305@gmail.com>
2020-11-06 05:24:35 +0000

    Update task22.js

--- task22.js
+++ task22.js
@@ -3,17 +3,17 @@ var task04=require('./task04');
 function deleteDirectory(pathToDirectory){
 return new Promise((res,rej)=>{
 fs.rmdir(pathToDirectory,error=>{
-//var checkExist=task04.checkFileExists(pathToDirectory);    
+var checkExist=task04.checkFileExists(pathToDirectory);    
 if(error){
 if(checkExist===true){    
 rej('cannot remove directory because : File does not exists')
 }
-
+}
 else{
 res('File exists : Directory removed');
 
 }
-}
+
 });
 
 
