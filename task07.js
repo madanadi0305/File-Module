773216ffbf7ae1617ea7c6f1b159ffe34d19c7b5
madanadi0305 <madanadi0305@gmail.com>
2020-11-06 16:02:06 +0000

    Update task07.js

--- task07.js
+++ task07.js
@@ -1,56 +1,31 @@
 var fs=require('fs');
-function fetchFilesAndDirectories(dirPath){
-var countDir=0;
-var countFile=0;
-var fileObj={}; 
+function countDirAndFiles(pathToDirectory){
 return new Promise((res,rej)=>{
-fs.readdir(dirPath,(err,pathContents)=>{
-if(err){
+fs.readdir(pathToDirectory,(error,files)=>{
+if(error){
     rej("Error occured while reading directory");
 }
 else{
-for(var i=0;i<pathContents.length;i++){
-var pathElem=pathContents[i];
-var countOfPath=checkFileOrDirectory(pathElem);
-countDir=countDir+countOfPath.dirCount;
-countFile=countFile+countOfPath.fileCount;
+    var countDir=0;
+    var countFile=0;
+ var fetchObj={};   
+for(var i=0;i<files.length;i++){
+var pathElem=files[i].toString();
+if(pathElem.includes(".")){
+    countFile=countFile+1;
 }
+else{
+    countDir=countDir+1;
+}
+}
+fetchObj.countDir=countDir;
+fetchObj.countFile=countFile;
+fetchObj.files=files;
+res(fetchObj);
+}
+});
 
-   
-fileObj.countDir=countDir;
-fileObj.countFile=countFile;
-fileObj.files=pathContents;
-res(fileObj);
-}
 
 });
 
-});
-
-
-}
-
-function checkFileOrDirectory(pathToDirectory){
-var dirCount=0;
-var fileCount=0;
-var countObj={};
-//return new Promise((res,rej)=>{
-fs.stat(pathToDirectory,(err,stats)=>{
-    if(err){
-        console.log(err.message);
-    }
-    else{
-   if(stats.isFile()){
-   fileCount=fileCount+1;
-   }
-       else{
-           dirCount=dirCount+1;
-       }
-
-    }
-});
-countObj.dirCount=dirCount;
-countObj.fileCount=fileCount;
-//});
-return countObj;
 }
