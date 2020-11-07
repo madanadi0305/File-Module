2be02475019f48f37a1b3b3560f513a3d709f069
madanadi0305 <madanadi0305@gmail.com>
2020-11-07 12:07:20 +0000

    Update task07.js, Wiki/17-task07.md files

--- task07.js
+++ task07.js
@@ -1,30 +1,38 @@
 var fs=require('fs');
-function countDirAndFiles(pathToDirectory){
-var countDir=0;
-var countFile=0;
-var filesObj={};
+function fetchAndCount(dirPath){
 return new Promise((res,rej)=>{
-fs.readdir(pathToDirectory,(error,files)=>{
-if(error){
+    fs.readdir(dirPath,(error,files)=>{
+    if(error){
     rej("Error occured while reading directory");
-}
 
-
-for(var i=0;i<files.length;i++){
-var elem=String(files[i]);    
-if(elem.includes('.')){
-countFile=countFile+1;
-}
+    }
 else{
-    countDir=countDir+1;
-}
-}
-filesObj.countDir=countDir;
-filesObj.countFile=countFile;
-filesObj.files=files;
-res(filesObj);
+   var obj={};
+   var dirCount=0;
+   var fileCount=0;
+   for(var i=0;i<files.length;i++){
+       var file=files[i];
+       if(file.includes('.')){
+           fileCount=fileCount+1;
+       }
+    else{
+        dirCount=dirCount+1;
+    }
+   }
 
-});
-});
+obj.countDir=dirCount;
+obj.countFile=fileCount;
+obj.files=files;
+res(obj);
+
+
 }
-module.export=countDirAndFiles;
+
+})
+});
+
+
+
+
+}
+module.exports=fetchAndCount;
