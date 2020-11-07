651fe22327db398be4621fbbf976946e37dd1c86
madanadi0305 <madanadi0305@gmail.com>
2020-11-07 08:29:36 +0000

    Update task07.js

--- task07.js
+++ task07.js
@@ -1,16 +1,18 @@
 var fs=require('fs');
 function countDirAndFiles(pathToDirectory){
+var countDir=0;
+var countFile=0;
+var filesObj={};
 return new Promise((res,rej)=>{
 fs.readdir(pathToDirectory,(error,files)=>{
 if(error){
     rej("Error occured while reading directory");
 }
 else{
-var countDir=0;
-var countFile=0;
-var filesObj={};
+
 for(var i=0;i<files.length;i++){
-if(String(files[i]).includes('.')===true){
+var elem=String(files[i]);    
+if(elem.includes('.')){
 countFile=countFile+1;
 
 }
