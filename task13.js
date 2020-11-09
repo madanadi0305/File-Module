65fd906940a58732b1c8100082602a10e1d3000b
madanadi0305 <madanadi0305@gmail.com>
2020-11-09 03:47:20 +0000

    Update task13.js

---
+++ task13.js
@@ -0,0 +1,57 @@
+var fs=require('fs');
+function fetchAndGet(pathDirectory,index){
+var fileAtIndex;
+return new Promise((res,rej)=>{
+fs.readdir(pathDirectory,(err,files)=>{
+if(err){
+rej("Error occured while fetching files from Directory");
+
+}
+else{
+
+for(var i=0;i<files.length;i++){
+if(i===index){
+fs.access(files[index],err=>{
+if(err){
+    throw err;
+}
+else{
+    fileAtIndex=files[index];
+}
+})
+
+break;
+}
+
+}
+var obj={};
+var data;
+fs.readFile(fileAtIndex,'utf-8',(readContent,err)=>{
+if(err){
+    throw err;
+}
+else{
+data=readContent;
+}
+
+})
+obj.data=data;
+fs.stat(fileAtIndex,(err,stats)=>{
+if(err){
+    throw err;
+}
+obj.size=stats.size;
+obj.birthTime=stats.birthtimeMs;
+obj.filename=fileAtIndex
+})
+}
+
+})
+
+
+});
+
+
+}
+
+module.exports=fetchAndGet;
