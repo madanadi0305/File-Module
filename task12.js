082c07e8d32cd164209a7e35f92b64e3f4d2bc90
madanadi0305 <madanadi0305@gmail.com>
2020-11-09 06:00:03 +0000

    Update task12.js

--- task12.js
+++ task12.js
@@ -1,53 +1,43 @@
 var fs=require('fs');
 var path=require('path');
-function fetchData(dirPath,index){
-  return new Promise((res,rej)=>{
-  fs.readdir(dirPath,(error,files)=>{
-   if(error){
+function fetchAndReturn(dirPath,index){
+return new Promise((res,rej)=>{
+fs.readdir(dirPath,(err,files)=>{
+if(err){
   rej("Error occured while fetching files from Directory");
-
-   }
-  else{
-    var fileAtIndex;
-    for(var i=0;i<files.length;i++){
-   if(index===i){
-   fileAtIndex=files[i];
-   break;
-   }
-
-    }
-fs.access(fileAtIndex,error=>{
-if(error){
-  throw error;
 }
 else{
-  var obj={};
-  var pathToFile=String(dirPath+"/"+fileAtIndex);
-  fs.readFile(pathToFile,'utf-8',(err,data)=>{
-  if(err){
-    throw err;
-  }
-  
-    obj.data=data;
-    obj.filename=path.basename(pathToFile);
-
-    res(obj);
-  
-
-  })
+var obj={};
+var pos=0;
+for(var i=0;i<files.length;i++){
+if(i===index){
+pos=i;
+break;
 }
 
+
+}
+var filePath=dirPath+"/"+String(files[pos]);
+fs.readFile(filePath,'utf-8',(err,data)=>{
+if(err){
+  throw new Error('Cannot read file');
+}
+var readContent=data;
+obj.data=readContent;
+obj.filename=path.basename(filePath);
+res(obj);
 })
 
 
-  }
+}
 
-  })
 
-  });
+})  
+
+
+});
 
 
 
 }
-
-module.exports=fetchData;
+module.exports=fetchAndReturn;
