0627bbbf46b56196b887de6a8239dcc5338a85b1
madanadi0305 <madanadi0305@gmail.com>
2020-11-09 04:07:32 +0000

    Update task17.js

---
+++ task17.js
@@ -0,0 +1,42 @@
+var fs=require('fs');
+function appendData(pathToFile,writeContent){
+return new Promise((res,rej)=>{
+fs.access(pathToFile,err=>{
+if(err){
+    rej("File does not exists provide another name");
+}
+else{
+fs.readFile(pathToFile,'utf-8',(err,data)=>{
+if(err){
+throw new Error('Error reading file');
+
+}
+
+else{
+//var readContent=data;
+fs.writeFile(pathToFile,writeContent,err=>{
+if(err){
+    return 0;
+}
+else{
+    res("Content appended to file successfully");
+}
+})
+}
+
+
+})
+
+
+}
+
+
+
+});
+
+});
+
+module.exports=appendData;
+
+
+}
