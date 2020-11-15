45d009d583d524e100786a3ce2f43af577fce497
madanadi0305 <madanadi0305@gmail.com>
2020-11-15 10:36:05 +0000

    Update task19.js

--- task19.js
+++ task19.js
@@ -2,9 +2,9 @@ var path=require('path');
 var fs=require('fs');
 var task4=require('./task04');
 function copyFileContents(sourceFile,destinationFile){
- var destPath=path.basename(destinationFile);   
+ //var destPath=path.basename(destinationFile);   
 return new Promise((res,rej)=>{
-task4(destPath).then(()=>{
+task4(destinationFile).then(()=>{
  rej("cannot copy data : File exists");
 }).catch(()=>{
 fs.copyFile(sourceFile,destinationFile,err=>{
