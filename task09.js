6881baa4a6d50289dade21998f42f439fa56b096
madanadi0305 <madanadi0305@gmail.com>
2020-11-16 13:05:24 +0000

    Update task09.js

---
+++ task09.js
@@ -0,0 +1,38 @@
+var fs=require('fs');
+var path=require('path');
+function countFilesAndDirectories(dirPath){
+var countDirs=0;
+var countFiles=0;
+var countObj={};
+return new Promise((res,rej)=>{
+fs.readdir(dirPath,(err,files)=>{
+if(err){
+    rej("Error occured while traversing directory");
+}
+for(var i=0;i<files.length;i++){
+var elem=files[i];
+var elemPath=String(dirPath)+"/"+String(elem);
+fs.stats(elemPath,(err,stats)=>{
+if(err){
+    throw err;
+}
+else{
+if(stats.isFile()){
+    countFiles=countFiles+1;
+}
+countDirs=countDirs+1;
+}
+})
+}
+countObj.countDir=parseInt(countDirs);
+countObj.countFile=parseInt(countFiles);
+res()
+})
+
+
+});
+
+
+}
+
+module.exports=countFilesAndDirectories;
