4cf1cceb19f7ea770a7756c244b047ddd3307681
madanadi0305 <madanadi0305@gmail.com>
2020-11-16 13:17:33 +0000

    Update task09.js

--- task09.js
+++ task09.js
@@ -11,7 +11,7 @@ if(err){
 }
 for(var i=0;i<files.length;i++){
 var elem=files[i];
-var elemPath=path.basename(dirPath+"/"+String(elem));
+var elemPath=dirPath+"/"+String(elem);
 fs.stat(elemPath,(err,stats)=>{
 if(err){
     throw err;
