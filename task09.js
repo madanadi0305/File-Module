776d400e2ebe5a4d9c9985eb3c6b9d64cacb2f19
madanadi0305 <madanadi0305@gmail.com>
2020-11-22 14:39:32 +0000

    Update task09.js

--- task09.js
+++ task09.js
@@ -18,11 +18,11 @@ var pathElem=files[i];
 if(pathElem.lastIndexOf('.')!==-1){
     countFile=countFile+1;
 }
-countSubDir=countSubDir+1;
-count(directoryPath+"/"+String(pathElem));
-}
-//countDir=countDir+1;
+//countSubDir=countSubDir+1;
 //count(directoryPath+"/"+String(pathElem));
+}
+countDir=countDir+1;
+count(directoryPath+"/"+String(pathElem));
 }
 countObj.countDir=parseInt(countSubDir);
 countObj.countFile=countFile;
