ba4facfdfff5f73e55790fdc5a78a50c7c6d4f31
madanadi0305 <madanadi0305@gmail.com>
2020-11-22 15:00:07 +0000

    Update task14.js

--- task14.js
+++ task14.js
@@ -1,37 +1,34 @@
 var fs=require('fs');
 var path=require('path');
-function getCount(filePath){
+function funcCount(filePath){
 return new Promise((res,rej)=>{
 fs.readFile(filePath,'utf-8',(err,data)=>{
 if(err){
     rej("Error reading file");
 }
+else{
+var functionCount=0;
+var variableCount=0;    
 var countObj={};
-var funcCount=0;
-var varCount=0;
-var readContent=String(data).split(' ');
-
-for(var i=0;i<readContent.length;i++){
-var content=readContent[i];    
-if(content==='function'||content==='=>'){
-funcCount=funcCount+1;
-
+var contents=data.toString();
+for(var content of contents){
+if(content.toString()==='){...}'||content.toString()===')=>{..}'){
+    functionCount=functionCount+1;
 }
-else if(content==='var'||content==='let'||content==='const'){
-varCount=varCount+1;
-
+else if(content.toString()==='var'||content.toString()==='let'||content.toString()==='const'){
+    variableCount=variableCount+1;
 }
-
 }
-countObj.functionCount=funcCount;
-countObj.variableCount=varCount;
+countObj.functionCount=functionCount;
+countObj.variableCount=variableCount;
 res(countObj);
+}
+
 })
 
 
-}); 
-
+});
 
 
 }
-module.exports=getCount;
+module.exports=funcCount;
