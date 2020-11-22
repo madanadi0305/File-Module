5d338d9f17617ffed5853e26c223a626a14ba634
madanadi0305 <madanadi0305@gmail.com>
2020-11-22 17:01:20 +0000

    Update task14.js

--- task14.js
+++ task14.js
@@ -13,7 +13,7 @@ var countObj={};
 var contents=data;
 for(var i=0;i<contents.length;i++){
     var content=contents[i];
-if((String(content)==='){')||(String(content)===')=>')){
+if((content==='){')||(content===')=>')){
     functionCount=functionCount+1;
 }
 else if((String(content)==='var')||(String(content)==='let')||(String(content)==='const')){
