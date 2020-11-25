c190b471495f756f64e5dac4458d141d5d70f246
madanadi0305 <madanadi0305@gmail.com>
2020-11-25 05:44:12 +0000

    Update task20.js

--- task20.js
+++ task20.js
@@ -1,22 +1,19 @@
-const fs=require('fs');
-//const path=require('path');
-const checkExist=require('./task04.js');
-function appendContent(filePath,content){
- //var data=String(content);   
-return new Promise((res,rej)=>{
-checkExist(filePath).then(msg=>
- {fs.appendFile(filePath,content,err=>{
-if(!err){
-res("Data appended successfully");
+var fs=require('fs');
+var checkExist=require('./task04');
+function appendContent(file,content){
+return new Promise((resolve,reject)=>{
+checkExist(file).then(
+fs.appendFile(file,content,err=>{
+if(err){
+    reject(err);
 }
-else { rej(new Error('Cannot append data'))};
- })
- }).catch(err=>
-{rej("Cannot append data because : File does not exists");
-//console.log(err);
-}
+resolve("Data appended successfully");
+})
 
-);
+).catch
+(
+    reject("Cannot append data because : File does not exists")
+    );
 
 
 });
