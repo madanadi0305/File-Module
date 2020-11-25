606578be334aad75c28c379c66bcaf29b6899159
madanadi0305 <madanadi0305@gmail.com>
2020-11-25 06:41:15 +0000

    Update task20.js

--- task20.js
+++ task20.js
@@ -1,21 +1,25 @@
-var fs=require('fs');
-var checkExist=require('./task04');
+const fs=require('fs');
+const checkExist=require('./task04');
 function appendContent(file,content){
 return new Promise((resolve,reject)=>{
-checkExist(file).then(result=>{
+checkExist(file)
+.then((result)=>{
 fs.appendFile(file,content,err=>{
 if(!err){
 resolve("Data appended successfully");
     //throw (err);
 }
-else{reject(err);}
+else{
+    reject(err);
+    }
+
+});
 
 })
-
-}).catch(err=>{
+.catch((err)=>{
     reject("Cannot append data because : File does not exists");
 
-})
+});
 
 
 });
@@ -23,3 +27,24 @@ else{reject(err);}
 
 }
 module.exports=appendContent;
+
+
+/*const taskFour = require('./task04');
+const fs = require('fs');
+
+function main(filePath, data) {
+  return new Promise((resolve, reject) => {
+    taskFour(filePath)
+      .then((response) => {
+        fs.appendFile(filePath, data, (err) => {
+          if (!err) resolve('Data appended successfully');
+          else reject('Error appending');
+        });
+      })
+      .catch((err) => {
+        reject('Cannot append data because : File does not exists');
+      });
+  });
+}
+
+module.exports = main;*/
